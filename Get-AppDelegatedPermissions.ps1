b9cd9af8c977465810f0e0920c4d4ad201efd5d0
Wim van den Heijkant <wim@fortigi.nl>
2020-08-19 14:55:52 +0200

    Fix..

--- Get-AppDelegatedPermissions.ps1
+++ Get-AppDelegatedPermissions.ps1
@@ -3,30 +3,27 @@ Connect-AzureAD
 
 #Create array we will use to gather data into.
 [array]$Report = $null
-
-
+ 
 #Get all service principles
 $SPs = Get-AzureADServicePrincipal -All $true
-
-
+ 
 #Get Application Permissions (access without a user)
 Foreach ($SP in $SPs) {
-
+ 
     [array]$AppOAuth2Permissions = Get-AzureADServicePrincipalOAuth2PermissionGrant -ObjectId $SP.ObjectID
-
+ 
     Foreach ($AppOAuth2Permission in $AppOAuth2Permissions) {
     
         $Principal = $null
         If ($AppOAuth2Permission.PrincipalId) {
             $Principal = Get-AzureADObjectByObjectId -ObjectIds $AppOAuth2Permission.PrincipalId -ErrorAction SilentlyContinue
         }
-
+ 
         if ($Principal) {
-
             $Resource = Get-AzureADObjectByObjectId -ObjectIds $AppOAuth2Permission.ResourceId
             $Application = Get-AzureADObjectByObjectId -ObjectIds $AppOAuth2Permission.ClientId
             [array]$Scopes = $AppOAuth2Permission.Scope.Split(" ")
-
+ 
             Foreach ($Scope in $Scopes) {
                 $Row = New-Object PSObject
                 $Row | add-member Noteproperty App                    $Application.DisplayName
@@ -34,11 +31,11 @@ Foreach ($SP in $SPs) {
                 $Row | add-member Noteproperty Permission             $Scope
                 $Row | add-member Noteproperty user                   $Principal.UserPrincipalName
                 $Row | add-member Noteproperty ExpiryTime             $AppOAuth2Permission.ExpiryTime
-
+ 
                 $Report += $Row
-                }
             }
-
+        }
+ 
     }
 }
 
@@ -46,11 +43,11 @@ Write-Host "Full List"
 $Report | Format-Table
 
 Write-Host "Filter for active.."
-$ActiveReport = $Report | Where-Object {$_.ExpiryTime -gt (Get-Date)}
+$ActiveReport = $Report | Where-Object { $_.ExpiryTime -gt (Get-Date) }
 
 Write-Host "Filter for . meaning all of something.. all mail, all files.. you profile.. not just your email adres or your id"
-$ActiveReport | Where-Object {$_.Permission.contains(".")} | Format-Table
+$ActiveReport | Where-Object { $_.Permission.contains(".") } | Format-Table
 
 Write-Host "Filterd, specificaly for Mail. or Files."
-$ActiveReport | Where-Object {$_.Permission.contains("Mail.") -or $_.Permission.contains("Files.")} | Format-Table
+$ActiveReport | Where-Object { $_.Permission.contains("Mail.") -or $_.Permission.contains("Files.") } | Format-Table
 
